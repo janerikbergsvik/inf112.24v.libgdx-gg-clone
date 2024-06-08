c94f8b102a2085900040b7b508fe3e8ef85e5e35
Jan Erik Bergsvik <jbergsvik@delmarsystems.com>
2024-06-08 15:46:11 +0200

    ..

--- target/classes/levels/tilesets_scaled_32x32/additional_types.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset_gutter 34" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="924" columns="33">
- <image source="tileset_gutter 34.png" width="1122" height="952"/>
-</tileset>
--- target/classes/truetypefonts/readme.txt
+++
@@ -1,2 +0,0 @@
-https://www.kreativekorp.com/software/fonts/c64/
-
